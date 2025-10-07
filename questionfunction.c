15b5cee9eb035c1865b5bb82e24539e2a9652b04
R4zym <yossaran.p6.2.17.ccs@gmail.com>
2025-10-07 20:16:34 +0700

    Fuctions(update and delete)

--- questionfunction.c
+++ questionfunction.c
@@ -14,7 +14,7 @@ typedef struct {
     char type[60];
     char department[60];
     char date[30];
-    char activate[8];
+    char activate[2];
 } Question;   
 
 void updatequestion();
@@ -34,29 +34,37 @@ void openfile(){
     }
     
     printf("\n====================================================================================================================\n");
-    printf("                                                ข้อมูลคำถามจากไฟล์\n");
+    printf("                                                Question Data from File\n");
     printf("====================================================================================================================\n");
-    printf("%-8s | %-50s | %-25s | %-20s | %-15s | %-8s\n", "รหัส", "คำถาม", "ประเภท", "Department", "วันที่เพิ่ม", "สถานะ");
-    printf("--------------------------------------------------------------------------------------------------------------------\n");
 
     char line[LINE_SIZE];
 
-    // ข้าม header
-    fgets(line, sizeof(line), questiondata);
-
+    
+    int row = 0;
     while (fgets(line, sizeof(line), questiondata)) {
         line[strcspn(line, "\n\r")] = 0;
 
-        char *col1 = strtok(line, ",");
-        char *col2 = strtok(NULL, ",");
-        char *col3 = strtok(NULL, ",");
-        char *col4 = strtok(NULL, ",");
-        char *col5 = strtok(NULL, ",");
-        char *col6 = strtok(NULL, ",");
-        if (col1 && col2 && col3 && col4 && col5 && col6) {
-            printf("%-8s | %-50s | %-25s | %-20s | %-15s | %-8s\n", col1, col2, col3, col4, col5, col6);
+        if (row == 0) {
+            char *col1 = strtok(line, ",");
+            char *col2 = strtok(NULL, ",");
+            char *col3 = strtok(NULL, ",");
+            char *col4 = strtok(NULL, ",");
+            char *col5 = strtok(NULL, ",");
+            char *col6 = strtok(NULL, ",");
+            printf("%-8s | %-50s | %-25s | %-20s | %-15s | %-8s\n", col1 ? col1 : "", col2 ? col2 : "", col3 ? col3 : "", col4 ? col4 : "", col5 ? col5 : "", col6 ? col6 : "");
+            printf("--------------------------------------------------------------------------------------------------------------------\n");
+        } else {
+            char *col1 = strtok(line, ",");
+            char *col2 = strtok(NULL, ",");
+            char *col3 = strtok(NULL, ",");
+            char *col4 = strtok(NULL, ",");
+            char *col5 = strtok(NULL, ",");
+            char *col6 = strtok(NULL, ",");
+            printf("%-8s | %-50s | %-25s | %-20s | %-15s | %-8s\n", col1 ? col1 : "", col2 ? col2 : "", col3 ? col3 : "", col4 ? col4 : "", col5 ? col5 : "", col6 ? col6 : "");
         }
+        row++;
     }
+    
     printf("====================================================================================================================\n\n");
     
     pressEnterToContinue();
@@ -65,8 +73,8 @@ void openfile(){
 }
 
 void addquestion() {
-    int qid = 0, questiontypechoice, day, month, year;
-    char question[200] = "", type[60] = "", department[60] = "", date[30] = "", line[300], activate[8] = "1";
+    int qid = 0, questiontypechoice, day, month, year , questionDepartmentChoice;
+    char question[200] = "", type[60] = "", department[60] = "", date[30] = "", line[300], activate[2] = "1";
     time_t t;
     struct tm *tm_info;
 
@@ -76,28 +84,47 @@ void addquestion() {
         return;
     }
 
-    // เลือกประเภทคำถาม
+    
     do {
-        printf("-----------ประเภทคำถาม-----------\n");
-        printf("1. ปลายเปิด\n");
-        printf("2. ปลายปิด\n");
-        printf("3. กลับเมนูหลัก\n");
+        printf("-----------Department-----------\n");
+        printf("1. Management\n");
+        printf("2. Marketing\n");
+        printf("3. Sales\n");
+        printf("4. Human Resources\n");
+        printf("5. Finance & Accounting\n");
+        printf("6. Operations / Production\n");
+        printf("7. Customer Service\n");
+        printf("8. Other\n");
+        printf("9. กลับเมนูหลัก\n");
+        printf("--------------------------------\n");
         printf("เลือกประเภทคำถาม: ");
-        scanf("%d", &questiontypechoice);
+        scanf("%d", &questionDepartmentChoice);
         getchar();
-        if (questiontypechoice == 1) {
-            strcpy(type, "ปลายเปิด");
-        } else if (questiontypechoice == 2) {
-            strcpy(type, "ปลายปิด");
-        } else if (questiontypechoice == 3) {
+        if (questionDepartmentChoice == 1) {
+            strcpy(department, "Management");
+        } else if (questionDepartmentChoice == 2) {
+            strcpy(department, "Marketing");
+        } else if (questionDepartmentChoice == 3) {
+            strcpy(department, "Sales");
+        } else if (questionDepartmentChoice == 4) {
+            strcpy(department, "Human Resources");
+        } else if (questionDepartmentChoice == 5) {
+            strcpy(department, "Finance & Accounting");
+        } else if (questionDepartmentChoice == 6) {
+            strcpy(department, "Operations / Production");
+        } else if (questionDepartmentChoice == 7) {
+            strcpy(department, "Customer Service");
+        } else if (questionDepartmentChoice == 8) {
+            strcpy(department, "Other");
+        } else if (questionDepartmentChoice == 9) {
             printf("กลับเมนูหลัก\n");
             fclose(questiondata);
             pressEnterToContinue();
             return;
         } else {
-            printf("กรุณาเลือก 1-3\n");
+            printf("กรุณาเลือก 1-9\n");
         }
-    } while (questiontypechoice != 1 && questiontypechoice != 2);
+    } while (questionDepartmentChoice != 1 && questionDepartmentChoice != 2 && questionDepartmentChoice != 3 && questionDepartmentChoice != 4 && questionDepartmentChoice != 5 && questionDepartmentChoice != 6 && questionDepartmentChoice != 7 && questionDepartmentChoice != 8 && questionDepartmentChoice != 9);
 
     // หา id ล่าสุด
     int last_id = 0;
@@ -110,7 +137,7 @@ void addquestion() {
     }
     fclose(questiondata);
 
-    // รับคำถาม
+    
     printf("---------------เพิ่มคำถามใหม่---------------\n");
     printf("กรอกคำถาม: ");
     fgets(question, sizeof(question), stdin);
@@ -122,17 +149,17 @@ void addquestion() {
         return;
     }
 
-    // รับแผนก
-    printf("กรอกชื่อแผนก: ");
-    fgets(department, sizeof(department), stdin);
-    department[strcspn(department, "\n")] = 0;
-    if (strlen(department) == 0) {
-        printf("กรุณากรอกชื่อแผนก\n");
+    
+    printf("กรอกประเภทคำถาม: ");
+    fgets(type, sizeof(type), stdin);
+    type[strcspn(type, "\n")] = 0;
+    if (strlen(type) == 0) {
+        printf("กรุณากรอกประเภทคำถาม\n");
         pressEnterToContinue();
         return;
     }
 
-    // สร้างวันที่
+    
     time(&t);
     tm_info = localtime(&t);
     year = tm_info->tm_year + 1900;
@@ -140,7 +167,7 @@ void addquestion() {
     day = tm_info->tm_mday;
     sprintf(date, "%02d/%02d/%04d", day, month, year);
 
-    // เขียนไฟล์
+    
     questiondata = fopen("questiondata.csv", "a");
     if (questiondata == NULL) {
         printf("ไม่สามารถเปิดไฟล์ได้\n");
@@ -185,13 +212,7 @@ int load_from_file(Question list[], int *count) {
         line[strcspn(line, "\n")] = '\0';
         if (strlen(line) == 0) continue;
 
-        sscanf(line, "%15[^,],%199[^,],%59[^,],%59[^,],%29[^,],%7[^,\n]",
-                list[*count].id,
-                list[*count].question,
-                list[*count].type,
-                list[*count].department,
-                list[*count].date,
-                list[*count].activate);
+        sscanf(line, "%15[^,],%199[^,],%59[^,],%59[^,],%29[^,],%7[^,\n]", list[*count].id, list[*count].question, list[*count].type, list[*count].department, list[*count].date, list[*count].activate);
         (*count)++;
     }
     fclose(questiondata);
@@ -270,13 +291,130 @@ void searchquestion() {
 }
 
 void updatequestion(){
-    // ยังไม่ได้ปรับปรุงเต็มรูปแบบ
-    printf("ฟังก์ชันแก้ไขคำถามยังไม่พร้อมใช้งาน\n");
+    Question list[MAX_Q];
+    int count = 0;
+    if (!load_from_file(list, &count) || count == 0) {
+        printf("ไม่มีคำถามในระบบ\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    char id[16];
+    printf("กรอก ID คำถามที่ต้องการแก้ไข: ");
+    scanf("%15s", id);
+    getchar();
+
+    int found = 0;
+    for(int i = 0; i < count; i++) {
+        if(strcmp(list[i].id, id) == 0) {
+            found = 1;
+            printf("พบคำถาม: %s\n", list[i].question);
+            printf("ประเภทคำถามเดิม: %s\n", list[i].type);
+
+            printf("กรอกคำถามใหม่ (หากไม่ต้องการเปลี่ยนแปลง กด Enter): \n");
+            char new_question[200];
+            fgets(new_question, sizeof(new_question), stdin);
+            new_question[strcspn(new_question, "\n")] = 0;
+            if (strlen(new_question) > 0) {
+                strcpy(list[i].question, new_question);
+            }
+
+            printf("กรอกประเภทคำถามใหม่ (หากไม่ต้องการเปลี่ยนแปลง กด Enter): \n");
+            char new_type[60];
+            fgets(new_type, sizeof(new_type), stdin);
+            new_type[strcspn(new_type, "\n")] = 0;
+            if (strlen(new_type) > 0) {
+                strcpy(list[i].type, new_type);
+            }
+
+            printf("สถานะเดิม: %s\n", list[i].activate);
+            printf("กรอกสถานะใหม่ (1=ใช้งาน, 0=ไม่ใช้งาน) (หากไม่ต้องการเปลี่ยนแปลง กด Enter): \n");
+            char new_activate[2];
+            fgets(new_activate, sizeof(new_activate), stdin);
+            new_activate[strcspn(new_activate, "\n")] = 0;
+            if (strlen(new_activate) > 0) {
+                strcpy(list[i].activate, new_activate);
+            }
+
+            time_t t;
+            struct tm *tm_info;
+            time(&t);
+            tm_info = localtime(&t);
+            int year = tm_info->tm_year + 1900;
+            int month = tm_info->tm_mon + 1;
+            int day = tm_info->tm_mday;
+            sprintf(list[i].date, "%02d/%02d/%04d", day, month, year);
+            strncpy(list[i].date, list[i].date, sizeof(list[i].date)-1);
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("ไม่พบคำถามที่มี ID นี้\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    FILE *questiondata = fopen("questiondata.csv", "w");
+    if(!questiondata) {
+        printf("ไม่สามารถเปิดไฟล์เพื่อเขียนได้\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    fprintf(questiondata, "id,question,type,department,date,activate\n");
+
+    for(int i= 0; i < count; i++) {
+        fprintf(questiondata, "%s,%s,%s,%s,%s,%s\n", list[i].id, list[i].question, list[i].type, list[i].department, list[i].date, list[i].activate);
+    }
+    fclose(questiondata);
+
+    printf("แก้ไขคำถามเรียบร้อย\n");
     pressEnterToContinue();
 }
 
 void deletequestion(){
-    // ยังไม่ได้ปรับปรุงเต็มรูปแบบ
-    printf("ฟังก์ชันลบคำถามยังไม่พร้อมใช้งาน\n");
+    Question list[MAX_Q];
+    int count = 0;
+    char id[16];
+    if (!load_from_file(list, &count) || count == 0) {
+        printf("ไม่มีคำถามในระบบ\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    printf("กรุณากรอกรหัสคำถามที่ต้องการลบ : ");
+    scanf("%15s", id);
+    getchar();
+
+    int found = 0;
+    for(int i = 0; i < count; i++) {
+        if(strcmp(list[i].id, id) == 0) {
+            strcpy(list[i].activate, "0");
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found) {
+        printf("ไม่พบรหัสคำถามนี้\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    FILE *questiondata = fopen("questiondata.csv", "w");
+    if(!questiondata) {
+        printf("ไม่สามารถเปิดไฟล์เพื่อเขียนได้\n");
+        pressEnterToContinue();
+        return;
+    }
+
+    fprintf(questiondata, "id,question,type,department,date,activate\n");
+    for(int i = 0; i < count; i++) {
+        fprintf(questiondata, "%s,%s,%s,%s,%s,%s\n", list[i].id, list[i].question, list[i].type, list[i].department, list[i].date, list[i].activate);
+    }
+    fclose(questiondata);
+
+    printf("ลบคำถามเรียบร้อย\n");
     pressEnterToContinue();
 }
